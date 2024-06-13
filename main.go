03dabfb451a1813580d514d2ae9f77643e90c0f0
Ada <ada@gnous.eu>
2024-06-14 00:07:26 +0200

    Add support for config path change
    Now, first cmdline argument is path for config, default is config.toml

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"git.xdol.org/xdol/xtrace/internal/config"
 )
 
 func main() {
-	c, err := config.LoadToml("config.toml")
+	var configPath string
+	switch len(os.Args) {
+	case 2: //nolint:mnd
+		configPath = os.Args[1]
+	case 1:
+		configPath = "config.toml"
+	default:
+		log.Fatal("Max 1 argument is valid.")
+	}
+
+	c, err := config.LoadToml(configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	c.HTTP.Server()
 }
